a35cf8acf46b625d593251b21b8ac84125e0a2e5
shady <shadyrajaab@gmail.com>
2024-04-09 14:20:45 -0300

    feat: adicionar seleção de consultores

--- main.py
+++ main.py
@@ -22,88 +22,102 @@ CONCLUIDOS_FIXA = ["ATIVO"]
 planilha = st.file_uploader("Planilha", type=["xlsx"])
 
 if planilha:
-    MOVEL = pd.read_excel(planilha, index_col=[0], sheet_name="MOVEL")
-    FIXA = pd.read_excel(planilha, index_col=[0], sheet_name="FIXA_E_AVANÇADA")
+    if "movel" not in st.session_state:
+        st.session_state.movel = pd.read_excel(
+            planilha, index_col=[0], sheet_name="MOVEL"
+        )
+        st.session_state.movel = st.session_state.movel[
+            ["CONSULTOR", "STATUS", "R$ ACUMULADO", "TIPO VENDA"]
+        ]
 
-    MOVEL = MOVEL[MOVEL["STATUS"].isin(CONCLUIDOS_MOVEL)]
-    FIXA = FIXA[FIXA["STATUS"].isin(CONCLUIDOS_FIXA)]
+    if "fixa" not in st.session_state:
+        st.session_state.fixa = pd.read_excel(
+            planilha, index_col=[0], sheet_name="FIXA_E_AVANÇADA"
+        )
+        st.session_state.fixa["R$ ACUMULADO"] = (
+            st.session_state.fixa["QUANTIDADE"]
+            * st.session_state.fixa["VALOR DO PLANO"]
+        )
+        st.session_state.fixa.rename(
+            columns={"TIPO DE VENDA": "TIPO VENDA"}, inplace=True
+        )
+        st.session_state.fixa = st.session_state.fixa[
+            ["CONSULTOR", "STATUS", "R$ ACUMULADO", "TIPO VENDA"]
+        ]
 
-    FIXA["R$ ACUMULADO"] = FIXA["QUANTIDADE"] * FIXA["VALOR DO PLANO"]
-    FIXA.rename(columns={"TIPO DE VENDA": "TIPO VENDA"}, inplace=True)
-
-    MOVEL = MOVEL[["CONSULTOR", "STATUS", "R$ ACUMULADO", "TIPO VENDA"]]
-    FIXA = FIXA[["CONSULTOR", "STATUS", "R$ ACUMULADO", "TIPO VENDA"]]
-
-    dataframe = pd.concat([FIXA, MOVEL])
+    dataframe = pd.concat([st.session_state.fixa, st.session_state.movel])
     dataframe.dropna(inplace=True)
 
-    faixas = []
+    dataframe = dataframe[dataframe["STATUS"].isin(CONCLUIDOS_MOVEL + CONCLUIDOS_FIXA)]
 
-    for consultor in dataframe["CONSULTOR"].unique().tolist():
-        df = dataframe[dataframe["CONSULTOR"] == consultor]
-        faixa = df["R$ ACUMULADO"].sum()
-        filtro = get_filtro(faixa)
+    consultor = st.selectbox(
+        "Selecionar Consultor", options=dataframe["CONSULTOR"].unique().tolist()
+    )
+    multiplicador_novos = st.number_input("Multiplicador Novos", step=0.1)
+    multiplicador_portabilidade = st.number_input(
+        "Multiplicador Portabilidade", step=0.1
+    )
+    multiplicador_ja_cliente = st.number_input("Multiplicador Já Cliente", step=0.1)
+    multiplicador_migracao_pre_pos = st.number_input(
+        "Multiplicador Migração Pré-Pós", step=0.1
+    )
+    multiplicador_fixa = st.number_input("Multiplicador Fixa", step=0.1)
+    multiplicador_avancada = st.number_input("Multiplicador Avançada", step=0.1)
 
-        novos_acumulado = df[df["TIPO VENDA"].isin(NOVOS)]["R$ ACUMULADO"].sum()
-        portabilidade_acumulado = df[df["TIPO VENDA"].isin(PORTABILIDADE)][
-            "R$ ACUMULADO"
-        ].sum()
-        ja_cliente_acumulado = df[df["TIPO VENDA"].isin(JA_CLIENTE)][
-            "R$ ACUMULADO"
-        ].sum()
-        migracao_pre_pos_acumulado = df[df["TIPO VENDA"].isin(MIGRACAO_PRE_POS)][
-            "R$ ACUMULADO"
-        ].sum()
-        avancada_acumulado = df[df["TIPO VENDA"] == "AVANÇADA"]["R$ ACUMULADO"].sum()
-        fixa_acumulado = df[df["TIPO VENDA"] == "FIXA"]["R$ ACUMULADO"].sum()
+    df = dataframe[dataframe["CONSULTOR"] == consultor]
+    faixa = df["R$ ACUMULADO"].sum()
+    filtro = get_filtro(faixa)
 
-        novos_multiplicador = MULTIPLICADORES.get(filtro)["NOVOS"]
-        portabilidade_multiplicador = MULTIPLICADORES.get(filtro)["PORTABILIDADE"]
-        ja_cliente_multiplicador = MULTIPLICADORES.get(filtro)["JÁ CLIENTE"]
-        migracao_pre_pos_multiplicador = MULTIPLICADORES.get(filtro)["MIGRAÇÃO PRÉ/PÓS"]
-        avancada_multiplicador = MULTIPLICADORES.get(filtro)["AVANÇADA"]
-        fixa_multiplicador = MULTIPLICADORES.get(filtro)["FIXA"]
+    novos_acumulado = df[df["TIPO VENDA"].isin(NOVOS)]["R$ ACUMULADO"].sum()
+    portabilidade_acumulado = df[df["TIPO VENDA"].isin(PORTABILIDADE)][
+        "R$ ACUMULADO"
+    ].sum()
+    ja_cliente_acumulado = df[df["TIPO VENDA"].isin(JA_CLIENTE)]["R$ ACUMULADO"].sum()
+    migracao_pre_pos_acumulado = df[df["TIPO VENDA"].isin(MIGRACAO_PRE_POS)][
+        "R$ ACUMULADO"
+    ].sum()
+    avancada_acumulado = df[df["TIPO VENDA"] == "AVANÇADA"]["R$ ACUMULADO"].sum()
+    fixa_acumulado = df[df["TIPO VENDA"] == "FIXA"]["R$ ACUMULADO"].sum()
 
-        faixas.append(
-            {
-                "CONSULTOR": consultor,
-                "FAIXA": faixa,
-                "FILTRO": filtro,
-                "*NOVOS": novos_multiplicador,
-                "*PORTABILIDADE": portabilidade_multiplicador,
-                "*JÁ CLIENTE": ja_cliente_multiplicador,
-                "*MIGRAÇÃO PRÉ/PÓS": migracao_pre_pos_multiplicador,
-                "*FIXA": fixa_multiplicador,
-                "*AVANÇADA": avancada_multiplicador,
-                "NOVOS": novos_acumulado,
-                "PORTABILIDADE": portabilidade_acumulado,
-                "JÁ CLIENTE": ja_cliente_acumulado,
-                "MIGRAÇÃO PRÉ/PÓS": migracao_pre_pos_acumulado,
-                "AVANÇADA": avancada_acumulado,
-                "FIXA": fixa_acumulado,
-                "R NOVOS": novos_acumulado * novos_multiplicador,
-                "R PORTABILIDADE": portabilidade_acumulado
-                * portabilidade_multiplicador,
-                "R JÁ CLIENTE": ja_cliente_acumulado * ja_cliente_multiplicador,
-                "R MIGRAÇÃO PRÉ/PÓS": migracao_pre_pos_acumulado
-                * migracao_pre_pos_multiplicador,
-                "R AVANÇADA": avancada_acumulado * avancada_multiplicador,
-                "R FIXA": fixa_acumulado * fixa_multiplicador,
-            }
-        )
+    remuneracoes = [
+        {
+            "CONSULTOR": consultor,
+            "FAIXA": faixa,
+            "FILTRO": filtro,
+            "*NOVOS": multiplicador_novos,
+            "*PORTABILIDADE": multiplicador_portabilidade,
+            "*JÁ CLIENTE": multiplicador_ja_cliente,
+            "*MIGRAÇÃO PRÉ/PÓS": multiplicador_migracao_pre_pos,
+            "*FIXA": multiplicador_fixa,
+            "*AVANÇADA": multiplicador_avancada,
+            "NOVOS": novos_acumulado,
+            "PORTABILIDADE": portabilidade_acumulado,
+            "JÁ CLIENTE": ja_cliente_acumulado,
+            "MIGRAÇÃO PRÉ/PÓS": migracao_pre_pos_acumulado,
+            "AVANÇADA": avancada_acumulado,
+            "FIXA": fixa_acumulado,
+            "R NOVOS": novos_acumulado * multiplicador_novos,
+            "R PORTABILIDADE": portabilidade_acumulado * multiplicador_portabilidade,
+            "R JÁ CLIENTE": ja_cliente_acumulado * multiplicador_ja_cliente,
+            "R MIGRAÇÃO PRÉ/PÓS": migracao_pre_pos_acumulado
+            * multiplicador_migracao_pre_pos,
+            "R AVANÇADA": avancada_acumulado * multiplicador_avancada,
+            "R FIXA": fixa_acumulado * multiplicador_fixa,
+        }
+    ]
 
-        soma_r = (
-            faixas[-1]["R NOVOS"]
-            + faixas[-1]["R PORTABILIDADE"]
-            + faixas[-1]["R JÁ CLIENTE"]
-            + faixas[-1]["R MIGRAÇÃO PRÉ/PÓS"]
-            + faixas[-1]["R AVANÇADA"]
-            + faixas[-1]["R FIXA"]
-        )
+    soma_r = (
+        remuneracoes[0]["R NOVOS"]
+        + remuneracoes[0]["R PORTABILIDADE"]
+        + remuneracoes[0]["R JÁ CLIENTE"]
+        + remuneracoes[0]["R MIGRAÇÃO PRÉ/PÓS"]
+        + remuneracoes[0]["R AVANÇADA"]
+        + remuneracoes[0]["R FIXA"]
+    )
 
-        faixas[-1]["SOMA R"] = soma_r
+    remuneracoes[0]["R TOTAL"] = soma_r
 
-    if len(faixas) >= 1:
-        df_faixas = pd.DataFrame(faixas)
+    df_r = pd.DataFrame(remuneracoes)
 
-    st.dataframe(df_faixas)
+    st.dataframe(df_r, hide_index=True)
+
