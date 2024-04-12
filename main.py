6ac7a9747f2c5f7d0b65e84b7463a93ade807633
shady <shadyrajaab@gmail.com>
2024-04-12 15:37:15 -0300

    feat: adicionar tabela de informações

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ from components.multipliers import multipliers
 from components.uploader import file_uploader
 from components.values import values
 from pdfhandler.generatepdf import build_pdf_file
+from pdfhandler.information import create_informatons_table
+from pdfhandler.multipliers import create_multipliers_table
 from pdfhandler.signature import create_signature_paragraph
 from pdfhandler.statement import create_statement_paragraph
-from pdfhandler.table import create_multipliers_table
 from session.state import load_session
 from utils.handler import handler_dataframe, handler_receita, handler_remuneracoes
-from utils.variables import DATA
+from utils.variables import DATA, DATA2
 
 st.set_page_config(layout="wide")
 
@@ -43,8 +44,9 @@ with c1:
         multipliers_table = create_multipliers_table(DATA)
         signature = create_signature_paragraph(st.session_state.consultor)
         statement = create_statement_paragraph()
+        informations = create_informatons_table(DATA2)
 
-        build_pdf_file(multipliers_table, statement, signature)
+        build_pdf_file([multipliers_table, informations, statement, signature])
 
         export_button()
 
