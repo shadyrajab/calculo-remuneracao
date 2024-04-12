bdb32e37567991d7e49e610a4841d920776a0cab
shady <shadyrajaab@gmail.com>
2024-04-12 11:37:27 -0300

    style: separar botões em colunas

--- components/filters.py
+++ components/filters.py
@@ -3,25 +3,22 @@ import streamlit as st
 
 def filters():
     if "planilha" in st.session_state and "dataframe" in st.session_state:
-        st.session_state.consultor = st.selectbox(
+        m1, m2, m3 = st.columns(3)
+        st.session_state.consultor = m1.selectbox(
             "Selecionar Consultor",
             options=st.session_state.dataframe["CONSULTOR"].unique().tolist(),
         )
-        st.session_state.xnovos = st.number_input(
-            "Multiplicador Novos", step=0.1
+        st.session_state.salario = m2.number_input("Salário do Consultor")
+        st.session_state.ajuda = m3.number_input("Ajuda de custo")
+
+        st.markdown("##### Multiplicadores:")
+        
+        c1, c2, c3, c4, c5, c6 = st.columns(6)
+        st.session_state.xnovos = c1.number_input("Novos", step=0.1)
+        st.session_state.xportabilidade = c2.number_input("Portabilidade", step=0.1)
+        st.session_state.xja_cliente = c3.number_input("Já Cliente", step=0.1)
+        st.session_state.xmigracao_pre_pos = c4.number_input(
+            "Migração Pré-Pós", step=0.1
         )
-        st.session_state.xportabilidade = st.number_input(
-            "Multiplicador Portabilidade", step=0.1
-        )
-        st.session_state.xja_cliente = st.number_input(
-            "Multiplicador Já Cliente", step=0.1
-        )
-        st.session_state.xmigracao_pre_pos = st.number_input(
-            "Multiplicador Migração Pré-Pós", step=0.1
-        )
-        st.session_state.xfixa = st.number_input(
-            "Multiplicador Fixa", step=0.1
-        )
-        st.session_state.xavancada = st.number_input(
-            "Multiplicador Avançada", step=0.1
-        )
+        st.session_state.xfixa = c5.number_input("Fixa", step=0.1)
+        st.session_state.xavancada = c6.number_input("Avançada", step=0.1)
